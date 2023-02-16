11a9e196ed34b2d92e07318305aadaac01fc2e7a
OdinsPlasmaRifle <generalaetius.jbvb@gmail.com>
2023-02-16 20:39:48 +0000

    Added new resource attaching functionality in mixins

--- drf_rehive_extras/mixins.py
+++ drf_rehive_extras/mixins.py
@@ -19,20 +19,22 @@ def add_resource_data(request, instance):
     available on the specific model instance.
     """
 
+    # Attempt to a set a resource on the request.
     try:
         resource = getattr(instance, "RESOURCE")
     except AttributeError:
         return
+    else:
+        request._resource = resource
 
-    request._resource = resource
-
+    # Attempt to a set a resource ID on the request.
     try:
         field = getattr(instance, "RESOURCE_ID")
         resource_id = getattr(instance, field)
     except AttributeError:
         return
-
-    request._resource_id = str(resource_id)
+    else:
+        request._resource_id = str(resource_id)
 
 
 class CreateModelMixin:
@@ -42,22 +44,26 @@ class CreateModelMixin:
 
     def create(self, request, *args, **kwargs):
         """
-        Generates a Response object with create status and response data.
-        Accepts optional `return_serializer` and return_status_code` kwargs that
-        will be used if set to override the serializer used in the response
-        data and response status code.
+        Handle object creation on the view.
         """
 
+        # Handle serialization and update.
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
+
+        # Inject resource data into the request.
+        add_resource_data(request, serializer.instance)
+
+        # Attach creation success headers.
         headers = self.get_success_headers(serializer.data)
 
+        # Handle the response serialization. Sometimes the serialization of
+        # responses should be different from the create serialization.
         _return_serializer = kwargs.get('return_serializer')
         data = _return_serializer(
             serializer.instance, context=self.get_serializer_context()
         ).data if _return_serializer else serializer.data
-        add_resource_data(request, serializer.instance)
 
         return Response(
             {'status': 'success', 'data': data},
@@ -66,9 +72,17 @@ class CreateModelMixin:
         )
 
     def perform_create(self, serializer):
+        """
+        Perform creation using the serializer.
+        """
+
         serializer.save()
 
     def get_success_headers(self, data):
+        """
+        Get special headers for the success response on object creation.
+        """
+
         try:
             return {'Location': data[api_settings.URL_FIELD_NAME]}
         except (TypeError, KeyError):
@@ -84,6 +98,10 @@ class ListModelMixin:
     pagination_class = PageNumberPagination
 
     def get_pagination_class(self):
+        """
+        Get a paginator class based on a pagination field in a GET param.
+        """
+
         paginator_name = self.request.GET.get('pagination')
 
         try:
@@ -97,7 +115,7 @@ class ListModelMixin:
     @property
     def paginator(self):
         """
-        Allow pagination to be overided via query params.
+        Fetch the correct paginator class.
         """
 
         if not hasattr(self, '_paginator'):
@@ -110,14 +128,22 @@ class ListModelMixin:
         return self._paginator
 
     def list(self, request, *args, **kwargs):
+        """
+        Handle object listing on the view.
+        """
+
+        # Get a list of objects as a queryset.
         queryset = self.filter_queryset(self.get_queryset())
 
+        # Paginate the queryset.
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
 
+        # Handle serialization.
         serializer = self.get_serializer(queryset, many=True)
+
         return Response({'status': 'success', 'data': serializer.data})
 
 
@@ -127,9 +153,19 @@ class RetrieveModelMixin:
     """
 
     def retrieve(self, request, *args, **kwargs):
+        """
+        Handle object retrieval on the view.
+        """
+
+        # Get a single object.
         instance = self.get_object()
+
+        # Inject resource data into the request.
+        add_resource_data(request, instance)
+
+        # Handle serialization.
         serializer = self.get_serializer(instance)
-        add_resource_data(request, serializer.instance)
+
         return Response({'status': 'success', 'data': serializer.data})
 
 
@@ -140,20 +176,27 @@ class UpdateModelMixin:
 
     def update(self, request, *args, **kwargs):
         """
-        Generates a Response object with response data.
-        Accepts optional `return_serializer` kwarg that will be used if set, to
-        override the serializer used in the response data.
+        Hanlde object update on the view.
         """
 
+        # Find out whether this is a partial (PATCH) or full (PUT) update.
         partial = kwargs.pop('partial', False)
+
+        # Get a single object.
         instance = self.get_object()
+
+        # Inject resource data into the request.
+        add_resource_data(request, instance)
+
+        # Handle serialization and update.
         serializer = self.get_serializer(
             instance, data=request.data, partial=partial
         )
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
-        add_resource_data(request, serializer.instance)
 
+        # Handle the response serialization. Sometimes the serialization of
+        # responses should be different from the update serialization.
         _return_serializer = kwargs.get('return_serializer')
         data = _return_serializer(
             serializer.instance, context=self.get_serializer_context()
@@ -162,9 +205,17 @@ class UpdateModelMixin:
         return Response({'status': 'success', 'data': data})
 
     def perform_update(self, serializer):
+        """
+        Perform update using the serializer.
+        """
+
         serializer.save()
 
     def partial_update(self, request, *args, **kwargs):
+        """
+        Perform a partial update (PATCH).
+        """
+
         kwargs['partial'] = True
         return self.update(request, *args, **kwargs)
 
@@ -175,13 +226,27 @@ class DestroyModelMixin:
     """
 
     def destroy(self, request, *args, **kwargs):
+        """
+        Handle object destruction on the view.
+        """
+
+        # Get a single object.
         instance = self.get_object()
+
+        # Inject resource data into the request.
+        add_resource_data(request, instance)
+
+        # Handle serialization and destroy.
         serializer = self.get_serializer(instance, data=request.data)
-        add_resource_data(request, serializer.instance)
         self.perform_destroy(serializer)
+
         return Response(
             data={'status': 'success'}, status=status.HTTP_200_OK
         )
 
     def perform_destroy(self, serializer):
+        """
+        Perform destroy using the serializer.
+        """
+
         serializer.destroy()
